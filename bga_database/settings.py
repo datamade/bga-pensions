8e3ea0b1f459403a17de31dfa7f606f0452795e3
hancush <hannah.cushman@gmail.com>
2019-07-01 12:06:46 -0500

    Set up local_settings

--- bga_database/settings.py
+++ bga_database/settings.py
@@ -12,20 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from .local_settings import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r#h9=3+#k1jqk52+d=8fx8v696vwm_iver9ar*=cue$_yt&#cv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,21 +71,6 @@ COMPRESS_ES6_COMPILER_CMD = (
     '{browserify_bin} "{infile}" -o "{outfile}" '
     '-t [ "{node_modules}/babelify" --presets="{node_modules}/babel-preset-env" ]'
 )
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bga_pensions',
-        'USER': 'hannah',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
