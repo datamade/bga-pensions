0ac3ba85eae851bc86e5f8de1718fd936026592d
hancush <hannah.cushman@gmail.com>
2019-07-02 14:33:00 -0500

    Use gpg2 in blackbox command

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -10,4 +10,4 @@ set -euo pipefail
 mkdir -p /home/datamade/bga-pensions
 
 # Decrypt files encrypted with blackbox
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade GPG=gpg2 blackbox_postdeploy
