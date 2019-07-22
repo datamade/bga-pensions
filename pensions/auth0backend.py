5e167c41c299e73367cf13595a358a0b1f336ac0
hancush <hannah.cushman@gmail.com>
2019-07-22 16:35:58 -0500

    Force email verification

--- pensions/auth0backend.py
+++ pensions/auth0backend.py
@@ -1,36 +1,34 @@
-from urllib import request
+from django.shortcuts import redirect
 from jose import jwt
-from social_core.backends.oauth import BaseOAuth2
+from social_core.backends.auth0 import Auth0OAuth2
+from social_core.exceptions import AuthFailed
 
 
-class Auth0(BaseOAuth2):
-    """Auth0 OAuth authentication backend"""
-    name = 'auth0'
-    SCOPE_SEPARATOR = ' '
-    ACCESS_TOKEN_METHOD = 'POST'
-    EXTRA_DATA = [
-        ('picture', 'picture')
-    ]
+class Auth0(Auth0OAuth2):
+    def auth_complete(self, *args, **kwargs):
+        try:
+            self.process_error(self.data)
+        except AuthFailed as e:
+            if 'verify your email' in str(e):
+                return redirect('/?verify_email=true')
+        else:
+            return super().auth_complete(*args, **kwargs)
 
-    def authorization_url(self):
-        return 'https://' + self.setting('DOMAIN') + '/authorize'
-
-    def access_token_url(self):
-        return 'https://' + self.setting('DOMAIN') + '/oauth/token'
-
-    def get_user_id(self, details, response):
-        """Return current user id."""
-        return details['user_id']
 
     def get_user_details(self, response):
         # Obtain JWT and the keys to validate the signature
         id_token = response.get('id_token')
-        jwks = request.urlopen('https://' + self.setting('DOMAIN') + '/.well-known/jwks.json')
-        issuer = 'https://' + self.setting('DOMAIN') + '/'
+        jwks = self.get_json(self.api_path('.well-known/jwks.json'))
+        issuer = self.api_path()
         audience = self.setting('KEY')  # CLIENT_ID
-        payload = jwt.decode(id_token, jwks.read(), algorithms=['RS256'], audience=audience, issuer=issuer)
+        payload = jwt.decode(id_token,
+                             jwks,
+                             algorithms=['RS256'],
+                             audience=audience,
+                             issuer=issuer)
 
         return {'username': payload['nickname'],
-                'first_name': payload['name'],
+                'email': payload['name'],
+                'email_verified': payload.get('email_verified', False),
                 'picture': payload['picture'],
                 'user_id': payload['sub']}
