ad7aa5548a06d124788c10de0a4bd45997b7dd74
hancush <hannah.cushman@gmail.com>
2019-08-08 15:37:54 -0500

    Add benefit distribution

--- pensions/views.py
+++ pensions/views.py
@@ -18,7 +18,7 @@ class Index(TemplateView):
 
     @property
     def data_years(self):
-        return list(range(2012, 2019))
+        return list(range(2012, 2020))
 #        if not hasattr(self, '_data_years'):
 #            self._data_years = Benefit.objects.distinct('data_year')\
 #                                              .values_list('data_year', flat=True)
@@ -29,6 +29,62 @@ class Index(TemplateView):
         if not hasattr(self, '_pension_funds'):
             self._pension_funds = PensionFund.objects.all()
         return self._pension_funds
+
+    def _binned_benefit_data(self):
+        DISTRIBUTION_BIN_NUM = 10
+        DISTRIBUTION_MAX = 250000
+
+        bin_size = DISTRIBUTION_MAX / DISTRIBUTION_BIN_NUM
+
+        with connection.cursor() as cursor:
+            cursor.execute('''
+                SELECT
+                  data_year,
+                  fund.name AS fund_name,
+                  width_bucket(amount, 0, 250000, 10) AS bucket_index,
+                  MAX(amount) AS max_value,
+                  COUNT(*)
+                FROM pensions_benefit AS benefit
+                JOIN pensions_pensionfund AS fund
+                ON benefit.fund_id = fund.id
+                GROUP BY data_year, fund.name, bucket_index
+                ORDER BY data_year, fund.name, bucket_index
+            ''')
+
+            binned_data = {}
+
+            for row in cursor:
+                data_year, fund, bucket_index, max_value, value = row
+                binned_data[(data_year, fund, bucket_index)] = (value, max_value)
+
+        benefit_json = {year: {} for year in self.data_years}
+
+        for year in benefit_json.keys():
+            year_data = {}
+
+            for fund in self.pension_funds:
+                fund_data = []
+
+                for i in range(DISTRIBUTION_BIN_NUM + 1):
+                    value, max_value = binned_data.get((year, fund.name, i + 1), (0, 0))
+
+                    lower = int(i * bin_size)
+                    upper = int(lower + bin_size)
+
+                    if i == DISTRIBUTION_BIN_NUM and max_value > upper:
+                        upper = max_value
+
+                    fund_data.append({
+                        'y': int(value),  # number of salaries in given bin
+                        'lower_edge': intword(lower),
+                        'upper_edge': intword(upper),
+                    })
+
+                year_data[fund.name] = fund_data
+
+            benefit_json[year] = year_data
+
+        return benefit_json
 
     def _aggregate_funding(self):
         '''
@@ -80,6 +136,8 @@ class Index(TemplateView):
         '''
         data_by_fund = {year: {} for year in self.data_years}
 
+        binned_benefit_data = self._binned_benefit_data()
+
         for fund in self.pension_funds.prefetch_related('annual_reports'):
             for annual_report in fund.annual_reports.all():
                 data_by_fund[annual_report.data_year][fund.name] = {
@@ -125,6 +183,11 @@ class Index(TemplateView):
                     'funding_level': int(annual_report.funded_ratio * 100),
                 }
 
+            for year in self.data_years:
+                fund_data = data_by_fund[year].get(fund.name, {})
+                fund_data['binned_benefit_data'] = binned_benefit_data[year][fund.name]
+                data_by_fund[year][fund.name] = fund_data
+
         return data_by_fund
 
     def _data_by_year(self):
@@ -158,7 +221,6 @@ class BenefitListJson(BaseDatatableView):
 
     # define the columns that will be returned
     columns = [
-        'fund',
         'first_name',
         'last_name',
         'amount',
@@ -188,9 +250,8 @@ class BenefitListJson(BaseDatatableView):
     def prepare_results(self, qs):
         json_data = []
 
-        for item in qs.select_related('fund'):
+        for item in qs:
             json_data.append([
-                item.fund.name,
                 item.first_name,
                 item.last_name,
                 item.amount,
